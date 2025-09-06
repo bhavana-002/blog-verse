b274e1ce495cdaeda8f9249c0ca97095f8e754d1
bhavana-002 <abhavana2k5@gmail.com>
2025-09-06 15:33:58 +0530

    hii

--- operators.js
+++ operators.js
@@ -60,3 +60,53 @@ console.log(d--)
 console.log(d)
 console.log(d)
 console.log(typeof d)
+let day=9;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    default:
+        console.log("invalid choice");
+        break;
+}
+let n=10;
+for(let i=0;i<=n;i++){
+    console.log(i)
+}
+let i=0;
+let m=10;
+while(i<m){
+    console.log(i)
+    i++;
+}
+let colors=["hi","hello","nope"]
+for(let i=0;i<colors.length;i++){
+    console.log(i);
+}
+for(let color in colors){
+    console.log(color);
+}
+let erson={
+    name : "bhavama",
+    age :19
+}
+for(let key in erson){
+    console.log(`{key}:${erson[key]}`);
+}
+//continue
+let l=9;
+for(let j=0;j<=l;j++){
+    if(j==3){
+        continue;
+    }
+    else{
+        break;
+    }
+    console.log(l)
+}
