dfd8f5942072958408413501ee906c7b5a2846c6
bhavana-002 <abhavana2k5@gmail.com>
2025-09-06 14:17:42 +0530

    hi

---
+++ sample.js
@@ -0,0 +1,25 @@
+console.log("Hello, Blog Verse!");
+
+let name = "Blog Verse";
+let num = 44;
+let islogged=false;
+let y=null;
+let arr = [1, 2, 3, 4, 5];
+
+console.log(`Welcome to ${name}!`);
+console.log(`The number is ${num}.`);
+console.log(typeof name);
+console.log(typeof islogged);
+console.log(typeof y);
+console.log(islogged);
+console.log(arr[0]);
+console.log(typeof arr);
+
+let person = {
+    name: "bhavana",
+    age: 19
+};
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(typeof person);
